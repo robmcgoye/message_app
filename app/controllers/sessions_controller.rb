3808643b2380297f87e1d8ca108322c32c25fbdc
Rob McGoye <rob@mcgoye.com>
2021-12-10 21:51:45 -0500

    Added login/logout authentication and flash messaging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,21 @@ class SessionsController < ApplicationController
   def new
   end
 
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Logged in successfully"
+      redirect_to root_path
+    else
+      flash.now[:error] = "There was something wrong your login details"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Logged out"
+    redirect_to login_path
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,4 +14,9 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-$(document).on("turbolinks:load", () => $(".ui.dropdown").dropdown());
+$(document).on("turbolinks:load", function() { 
+  $(".ui.dropdown").dropdown();
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
+})
