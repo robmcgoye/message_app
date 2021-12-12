0210fac0961eb7654263b79cc91f73c1e836cec4
Rob McGoye <rob@mcgoye.com>
2021-12-12 11:17:00 -0500

    Action cable implemenation to the chat form.

---
+++ app/javascript/channels/chatroom_channel.js
@@ -0,0 +1,17 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create("ChatroomChannel", {
+  connected() {
+    // Called when the subscription is ready for use on the server
+    //console.log("Connected to the chat room!");
+  },
+
+  disconnected() {
+    // Called when the subscription has been terminated by the server
+  },
+
+  received(data) {
+    $('#message-container').append(data.mod_message);
+  }
+
+});
